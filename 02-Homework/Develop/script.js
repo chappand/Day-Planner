9c0727a5c2ea6796d59e773cf24327aa3e313b05
Andrew Chappell <chappand@gmail.com>
2021-01-07 18:26:36 -0500

    Added Javascript file and some psuedo comments to help frame my thoughts.

---
+++ 02-Homework/Develop/script.js
@@ -0,0 +1,6 @@
+// Current Day populates at the top of the calendar when page is opened.
+// Standard business hour timeblock sit below the Current Day 
+// Time blocks are color coded for past, present, and future
+// Click into a time block and be able to enter an event 
+// Click save on the time block to save event to local storage. The saved event should stay on the page even if the page is refreshed.
+// Use moment.js for the date library

5c0e35bb6929a6f1be0673e8f73c5b991c1ffc7a
Andrew Chappell <chappand@gmail.com>
2021-01-07 22:52:07 -0500

    Added JS to add today's date to the top of the page.

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -4,3 +4,8 @@
 // Click into a time block and be able to enter an event 
 // Click save on the time block to save event to local storage. The saved event should stay on the page even if the page is refreshed.
 // Use moment.js for the date library
+
+
+// This JS adds today's date to the top of the planner
+document.getElementById("currentDay").textContent=moment().format("dddd, MMM DD YYYY");
+

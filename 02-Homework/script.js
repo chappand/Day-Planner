827e83f8f0696fc96dd1856c6978a8a61514fd3a
Andrew Chappell <chappand@gmail.com>
2021-01-07 23:57:19 -0500

    Just added a quick array.

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -12,12 +12,14 @@ var rightNow = moment().format("HH");
 // This JS adds today's date to the top of the planner
 document.getElementById("currentDay").textContent=thisDay;
 
+var array????? = list of work hours?
+
 for(each hour){
     append "form" row to the #container class;
     create a div with the .hour id to hold the hour of the row;
     create a div to hold the description of the row;
     create a button to save items with the .saveBtn id and saveBtn i:hover id;
-}
+};
 if (hour selected is less than current hour use the .past class);
 if (hour selected is equal to current hour use the .present class);
 if (hour selected is greater than current hour use the .future class);

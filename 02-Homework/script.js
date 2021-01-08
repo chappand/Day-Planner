baadfe4420bd286effc7ae4b9719b6f817aeba1e
Andrew Chappell <chappand@gmail.com>
2021-01-07 23:56:15 -0500

    Added Psuedo code for the planner.

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -1,4 +1,3 @@
-// Current Day populates at the top of the calendar when page is opened.
 // Standard business hour timeblock sit below the Current Day 
 // Time blocks are color coded for past, present, and future
 // Click into a time block and be able to enter an event 
@@ -12,6 +11,19 @@ var thisDay = moment().format("dddd, MMM DD YYYY");
 var rightNow = moment().format("HH");
 // This JS adds today's date to the top of the planner
 document.getElementById("currentDay").textContent=thisDay;
+
+for(each hour){
+    append "form" row to the #container class;
+    create a div with the .hour id to hold the hour of the row;
+    create a div to hold the description of the row;
+    create a button to save items with the .saveBtn id and saveBtn i:hover id;
+}
+if (hour selected is less than current hour use the .past class);
+if (hour selected is equal to current hour use the .present class);
+if (hour selected is greater than current hour use the .future class);
+
+on.click save information to localStorage;
+retrieve information from localStorage;
 
 
 });

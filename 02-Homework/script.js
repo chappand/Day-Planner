44ab9f7f10d0815f4da0be024ea499b96a8714c1
Andrew Chappell <chappand@gmail.com>
2021-01-09 14:11:25 -0500

    appended a single row with a box for the hour, text input, and save button.

--- 02-Homework/script.js
+++ 02-Homework/script.js
@@ -12,20 +12,35 @@ var rightNow = moment().format("HH");
 // This JS adds today's date to the top of the planner
 document.getElementById("currentDay").textContent=thisDay;
 
-var array????? = list of work hours?
+// var array????? = list of work hours?
 
-for(each hour){
-    append "form" row to the #container class;
-    create a div with the .hour id to hold the hour of the row;
-    create a div to hold the description of the row;
-    create a button to save items with the .saveBtn id and saveBtn i:hover id;
+// for(each hour){
+//     append a row to the #container class;
+//     create a div with the .hour id to hold the hour of the row;
+//     create a div to hold the text area with .description id of the row;
+//     create a button to save items with the .saveBtn id and saveBtn i:hover id;
+// };
+// if (hour selected is less than current hour use the .past class);
+// if (hour selected is equal to current hour use the .present class);
+// if (hour selected is greater than current hour use the .future class);
+
+// on.click save information to localStorage;
+// retrieve information from localStorage;
+
+// Add new Row with Content
+function rowContent() {
+
+var taskRow = $("<row>").attr("class", "row");
+var save = $("<button>SAVE</button>").attr("class", "saveBtn i:hover");
+var taskHour = $("<div>").attr("class", "hour col-md-1");
+var task = $("<textarea>").attr("class", "description col-md-9");
+
+
+$("#planner").append(taskRow);
+$(".row").append(taskHour);
+$(".row").append(task);
+$(".row").append(save);
+
 };
-if (hour selected is less than current hour use the .past class);
-if (hour selected is equal to current hour use the .present class);
-if (hour selected is greater than current hour use the .future class);
-
-on.click save information to localStorage;
-retrieve information from localStorage;
-
-
+rowContent();
 });
